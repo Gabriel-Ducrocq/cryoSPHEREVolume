fbdeaa1213f6ddfe06986f6a94d685e707bb2e86
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-07-31 21:29:37 +0200

    again

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def train(yaml_setting_path, debug_mode):
             alms_per_coordinate = utils.alm_from_radius_to_coordinate(alms_per_radius, radius_indexes)
             all_coordinates = model.grid.rotate_grid(batch_poses, freqs)
             all_sph = utils.get_real_spherical_harmonics(all_coordinates, sphericartObj, device, l_max)
-            predicted_images = utils.spherical_synthesis_hartley(alms_per_coordinate, all_sph)
+            predicted_images = utils.spherical_synthesis_hartley(alms_per_coordinate, all_sph, indexes)
             nll = loss.compute_loss(predicted_images, flattened_batch_images, latent_mean, latent_std, experiment_settings,
                                 tracking_metrics, experiment_settings["loss_weights"])
             nll.backward()
