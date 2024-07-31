a45514d44415524fc925c381b6b7c2d67e097ca8
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-07-31 15:11:06 +0200

    investigating again

--- model/utils.py
+++ model/utils.py
@@ -102,8 +102,6 @@ def get_real_spherical_harmonics(coordinates, sphericart_obj):
     :return: torch.tensor(N_batch, N_freqs)
     """
     coordinates = coordinates.reshape(-1, 3)
-    print("Coord")
-    print(coordinates)
     sh_values = sphericart_obj.compute(coordinates.detach().cpu().numpy())
     return sh_values
 
@@ -115,6 +113,9 @@ def alm_from_radius_to_coordinate(alm, radiuses_index):
     :param radiuses_index: torch.tensor(side_shape**2) of alm index corresponding to the radius of that coordinate
     :return: torch.tensor(N_batch, side_shape**2, (l_max+1)**2)
     """
+    print("ALLMALMALMALMALM")
+    print(alm.shape())
+    print(torch.max(radiuses_index))
     return alm[:, radiuses_index, :]
 
 def spherical_synthesis_hartley(alm_per_coord, spherical_harmonics):
