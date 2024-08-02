c7859b048c38b57b50327ed9b8bff658993908d8
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-08-02 19:09:06 +0200

    again

--- model/utils.py
+++ model/utils.py
@@ -239,7 +239,7 @@ def apply_wigner_D(wigner_matrices, spherical_harmonics, l_max):
     res = []
     for l in range(l_max+1):
         start = 0
-        print(wigner_matrices.shape)
+        print(wigner_matrices[l].shape)
         #r = torch.einsum("s l,b l e -> b s e", spherical_harmonics[:, start:start+(2*l+1)],wigner_matrices[l])
         r = torch.einsum("b e l , s l-> b s e", wigner_matrices[l], spherical_harmonics[:, start:start + (2 * l + 1)])
         start += 2*l+1
