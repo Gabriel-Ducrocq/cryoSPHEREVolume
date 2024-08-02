c3c8d0cbd73da50ff0bced29ecd671cf2e2fb258
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-08-02 19:14:16 +0200

    again

--- model/utils.py
+++ model/utils.py
@@ -300,7 +300,7 @@ rot_mat = pytorch3d.transforms.random_rotations(1, dtype=torch.float32, device=d
 rotated_grid = torch.einsum("b a q, b r q -> b r a", rot_mat, grid)
 result1 = get_real_spherical_harmonics(rotated_grid, sh, device, l_max)
 
-euler_angles = pytorch3d.transforms.matrix_to_euler_angles(rot_mat, "YXY")
+euler_angles = pytorch3d.transforms.matrix_to_euler_angles(torch.transpose(rot_mat, dim0=-1, dim1=-2), "YXY")
 euler_angles = euler_angles.detach().cpu()
 wigner_matrices = compute_wigner_D(l_max, euler_angles[:,0], euler_angles[:,1], euler_angles[:,2])
 spher = get_real_spherical_harmonics(grid, sh, device, l_max)
