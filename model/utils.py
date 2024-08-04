387057c75ede5bf4f46181fce03de135e03a8ecb
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-08-04 18:34:31 +0200

    again

--- model/utils.py
+++ model/utils.py
@@ -63,11 +63,11 @@ def parse_yaml(path):
                   experiment_settings["latent_dimension"] * 2,
                   experiment_settings["encoder"]["hidden_dimensions"], network_type="encoder", device=device,
                   latent_type="continuous")
-    #decoder = MLP(experiment_settings["latent_dimension"], N_unique_radiuses*(l_max+1)**2,
-    #              experiment_settings["decoder"]["hidden_dimensions"], network_type="decoder", device=device)
-
-    decoder = MLP(1, (l_max+1)**2,
+    decoder = MLP(experiment_settings["latent_dimension"], N_unique_radiuses*(l_max+1)**2,
                   experiment_settings["decoder"]["hidden_dimensions"], network_type="decoder", device=device)
+
+    #decoder = MLP(1, (l_max+1)**2,
+    #              experiment_settings["decoder"]["hidden_dimensions"], network_type="decoder", device=device)
 
     vae = VAE(encoder, decoder, device, latent_dim=experiment_settings["latent_dimension"], lmax=l_max)
     vae.to(device)
