2440317dcfd648745c1dd517afebd0b2be94365c
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-08-07 21:37:36 +0200

    fixing bug

--- data/analyze.py
+++ data/analyze.py
@@ -26,7 +26,7 @@ def analyze(yaml_setting_path, model_path, volumes_path):
     :return:
     """
     vae, optimizer, dataset, N_epochs, batch_size, sphericartObj, unique_radiuses, radius_indexes, experiment_settings, device, \
-        scheduler, freqs, freqs_volume, l_max, spherical_harmonics = utils.parse_yaml(
+        scheduler, freqs, freqs_volume, l_max, spherical_harmonics, wigner_calculator = utils.parse_yaml(
         yaml_setting_path)
 
     data_loader_std = iter(DataLoader(dataset, batch_size=10000, shuffle=False, num_workers=4, drop_last=True))
