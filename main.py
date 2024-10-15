5444153f2fd65ffa24556d19f6c4abe06d69656f
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-10-15 11:47:23 +0700

    fixing bug

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def train(yaml_setting_path, debug_mode):
             batch_poses = batch_poses.to(device)
             flattened_batch_images = batch_images.flatten(start_dim=1, end_dim=2)
             images_real = model.utils.real_to_hartley(batch_images)
-            batch_translated_images_real = image_translator.transform(images_real, batch_poses_translation[:, None, :]).flatten(start_dim=1, end_dim=2)
+            batch_translated_images_real = image_translator.transform(images_real, batch_poses_translation[:, None, :])
             batch_translated_images_hartley = model.utils.real_to_hartley(batch_translated_images_real)
             batch_translated_images_hartley = (batch_translated_images_hartley - images_mean)/(images_std + 1e-15)
             batch_translated_images_hartley = batch_translated_images_hartley.flatten(start_dim=1, end_dim=2)
