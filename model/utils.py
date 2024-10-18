c7ab26e9914626cb5ded7d2ae4942b38f0cb6af5
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-10-18 13:35:41 +0700

    fixing side shape

--- model/utils.py
+++ model/utils.py
@@ -182,7 +182,7 @@ def spherical_synthesis_hartley(alm_per_coord, spherical_harmonics, circular_mas
     spherical_harmonics[:, indexes ==0, :] = 0
     images_radius_0_nan = torch.einsum("b s l, b s l -> b s", alm_per_coord, spherical_harmonics)
     images_radius_0_nan[:, indexes == 0] = alm_per_coord[:, indexes == 0, 0]
-    flat_images = torch.zeros(batch_size, side_shape)
+    flat_images = torch.zeros(batch_size, side_shape**2)
     flat_images[:, circular_mask == 1] = images_radius_0_nan
     return flat_images.reshape(batch_size, side_shape, side_shape)
 
