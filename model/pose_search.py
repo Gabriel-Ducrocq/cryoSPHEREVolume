3f66a88cb47cff93da6c9a2d61bf3847a4a9c975
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-06 13:49:12 +0700

    fixing repeat

--- model/pose_search.py
+++ model/pose_search.py
@@ -22,7 +22,7 @@ def perform_pose_search(batch_translated_images_hartley, latent_mean, latent_std
 	poses_min[:, 0, 0] = poses_min[:, 1, 1] = poses_min[:, 2, 2] = 1
 	generated_images = torch.zeros(batch_size, int(np.sqrt(npix)), int(np.sqrt(npix)),  dtype=torch.float32, device=device)
 	for i, batch_poses in tqdm(enumerate(poses)):
-		batch_poses = batch_poses[None, :, :].repeat(batch_size, 1)
+		batch_poses = batch_poses[None, :, :].repeat(batch_size, 1, 1)
 		all_wigner = wigner_calculator.compute_wigner_D(l_max, batch_poses, device)
 		#all_wigner = utils.compute_wigner_D(l_max, batch_poses, device)
 		rotated_spherical_harmonics = utils.apply_wigner_D(all_wigner, spherical_harmonics, l_max)
