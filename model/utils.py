fixing ordering of the quaternions

model/utils.py
@@ -124,7 +124,7 @@ def parse_yaml(path):
         print("Perfoming pose search !")
         #Loading rotation poses from quaternions and set them as matrix:
         grid_rotations = np.load(experiment_settings["poses"])
-        grid_rotations = scipy.spatial.transform.Rotation.from_quat(grid_rotations, scalar_first=True)
+        grid_rotations = scipy.spatial.transform.Rotation.from_quat(grid_rotations[:, [1, 2, 3, 0]])
         grid_rotations = torch.tensor(grid_rotations.as_matrix(), dtype=torch.float32, device=device)
 
 