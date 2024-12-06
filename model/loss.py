7f48dc90af4afd48f20acb0a533b9b713a9e8ba1
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-12-06 16:10:49 +0100

    saving reg images

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,6 @@
 import torch
 import utils
+import matplotlib.pyplot as plt
 
 def calc_cor_loss(pred_images, gt_images, mask=None):
     """
@@ -71,6 +72,9 @@ def compute_loss(predicted_images, images, structural_predicted_particles, predi
     loss_regularization = 0
     if structural_predicted_particles is not None:
         predicted_images_real = utils.real_to_hartley(predicted_images_no_ctf)
+        im_example = predicted_images_real[0].detach().cpu().numpy()
+        plt.imshow(im_example)
+        plt.savefig("image_reg.png")
         rmsd_structural = compute_image_loss(predicted_images_real.flatten(start_dim=-2, end_dim=-1), structural_predicted_particles.flatten(start_dim=-2, end_dim=-1))
 
     tracking_dict["rmsd"].append(rmsd.detach().cpu().numpy())
