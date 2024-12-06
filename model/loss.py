73e15d3b52529c9b6d00fdb6f39b4c91ee9aa659
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-12-06 12:25:09 +0100

    running with only the regulirzation as loss

--- model/loss.py
+++ model/loss.py
@@ -69,12 +69,12 @@ def compute_loss(predicted_images, images, structural_predicted_particles, predi
         rmsd = compute_image_loss(images, predicted_images)
 
     loss_regularization = 0
-    #if structural_predicted_particles is not None:
-    #    predicted_images_real = utils.real_to_hartley(predicted_images_no_ctf)
-    #    rmsd_structural = compute_image_loss(predicted_images_real.flatten(start_dim=-2, end_dim=-1), structural_predicted_particles.flatten(start_dim=-2, end_dim=-1))
+    if structural_predicted_particles is not None:
+        predicted_images_real = utils.real_to_hartley(predicted_images_no_ctf)
+        rmsd_structural = compute_image_loss(predicted_images_real.flatten(start_dim=-2, end_dim=-1), structural_predicted_particles.flatten(start_dim=-2, end_dim=-1))
 
     tracking_dict["rmsd"].append(rmsd.detach().cpu().numpy())
-    #tracking_dict["rmsd_structural"].append(rmsd_structural.detach().cpu().numpy())
+    tracking_dict["rmsd_structural"].append(rmsd_structural.detach().cpu().numpy())
 
     loss = rmsd
-    return loss #+ 0.1*rmsd_structural
+    return 0*loss + rmsd_structural
