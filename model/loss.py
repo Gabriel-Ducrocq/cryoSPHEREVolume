e5e9e232f08d1740ff55248f53e6045475a60c01
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-02-06 06:23:47 +0100

    same weight to both loss terms

--- model/loss.py
+++ model/loss.py
@@ -77,4 +77,4 @@ def compute_loss(predicted_images, images, structural_predicted_particles_ht, pr
     tracking_dict["rmsd_structural"].append(rmsd_structural.detach().cpu().numpy())
 
     loss = rmsd
-    return loss + 0.5*rmsd_structural
+    return loss + rmsd_structural
