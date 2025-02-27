4cc3fa32f1963678f33c24ae8c8b60c0354fcfb3
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-02-27 09:38:36 +0100

    learning only predicted

--- model/loss.py
+++ model/loss.py
@@ -77,4 +77,5 @@ def compute_loss(predicted_images, images, structural_predicted_particles_ht, pr
     tracking_dict["rmsd_structural"].append(rmsd_structural.detach().cpu().numpy())
 
     loss = rmsd
-    return loss + 0.01*rmsd_structural
+    return rmsd_structural
+    #return loss + 0.01*rmsd_structural
